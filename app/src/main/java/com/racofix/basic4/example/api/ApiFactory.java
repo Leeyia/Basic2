869e3f79582ae0f00960eeaa34d89a6e6314e4e7
geeklux <geeklux@163.com>
2018-06-03 21:45:08 +0800

    fix

--- app/src/main/java/com/racofix/basic4/example/api/ApiFactory.java
+++ app/src/main/java/com/racofix/basic4/example/api/ApiFactory.java
@@ -9,24 +9,21 @@ import okhttp3.OkHttpClient;
 
 public class ApiFactory {
 
-    private static HttpRequest request;
-
     public static AccountServices account() {
         return servs(AccountServices.class);
     }
 
     private static <T> T servs(Class<T> clzz) {
-        if (request == null)
-            request = new HttpRequest.Builder()
-                    .baseUri("https://api.elabels.cn/v1/api/")
-                    .client(createClient())
-                    .build();
+        HttpRequest request = new HttpRequest.Builder()
+                .baseUri("https://api.elabels.cn/v1/api/")
+                .client(createClient())
+                .build();
         return HttpProxy.getInstance().newRequestQueue(clzz, request);
     }
 
     private static OkHttpClient createClient() {
-        HttpLogInterceptor logInterceptor =
-                new HttpLogInterceptor().setLevel(BuildConfig.DEBUG ? HttpLogInterceptor.Level.BODY : HttpLogInterceptor.Level.NONE);
+        HttpLogInterceptor logInterceptor = new HttpLogInterceptor()
+                .setLevel(BuildConfig.DEBUG ? HttpLogInterceptor.Level.BODY : HttpLogInterceptor.Level.NONE);
         return new OkHttpClient.Builder()
                 .addNetworkInterceptor(logInterceptor)
                 .build();
